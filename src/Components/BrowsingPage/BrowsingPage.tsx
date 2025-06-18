10f016332c19bbe75808bf546a2dce85034e2cbc
Marcell Meilinger <marcell.meilinger@gmail.com>
2025-06-18 20:11:38 +0200

    Updated BrowserPage, so it displays the right amount of cards responsively

--- src/Components/BrowsingPage/BrowsingPage.tsx
+++ src/Components/BrowsingPage/BrowsingPage.tsx
@@ -1,17 +1,16 @@
 import "./BrowsingPage.css";
-import { ItemCard } from "../../Components/ItemCard/ItemCard";
+import ItemCard from "../../Components/ItemCard/ItemCard";
 
 const BrowsingPage = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Browse Listings</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="max-w-full mx-auto px-4 py-8">
+      <div className="flex flex-wrap justify-center gap-4 max-w-[1440px] mx-auto">
         <ItemCard />
         <ItemCard />
         <ItemCard />
         <ItemCard />
         <ItemCard />
-        {/* Add more ItemCards as needed for mock layout */}
+        <ItemCard />
       </div>
     </div>
   );
