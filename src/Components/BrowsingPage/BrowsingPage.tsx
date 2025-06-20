825f59c08ec5dc3830cd2798d06b18161a2a03e0
Marcell Meilinger <marcell.meilinger@gmail.com>
2025-06-20 13:42:03 +0200

    Modified the BrowsingPage to use separate components

--- src/Components/BrowsingPage/BrowsingPage.tsx
+++ src/Components/BrowsingPage/BrowsingPage.tsx
@@ -1,10 +1,29 @@
-import "./BrowsingPage.css";
+import { useState } from "react";
+import SearchBar from "../../Components/SearchBar/SearchBar";
+import CategorySelector from "../../Components/CategorySelector/CategorySelector";
+import FilterDropdown from "../../Components/FilterDropdown/FilterDropdown";
 import ItemCard from "../../Components/ItemCard/ItemCard";
 
 const BrowsingPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Összes");
+  const [selectedFilter, setSelectedFilter] = useState("Legújabb feltöltés");
+
   return (
-    <div className="max-w-full mx-auto px-4 py-8">
-      <div className="flex flex-wrap justify-center gap-4 max-w-[1440px] mx-auto">
+    <div className="max-w-[1440px] mx-auto px-4 py-8 space-y-6">
+      <SearchBar />
+
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
+        <CategorySelector
+          selected={selectedCategory}
+          setSelected={setSelectedCategory}
+        />
+        <FilterDropdown
+          selected={selectedFilter}
+          setSelected={setSelectedFilter}
+        />
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4">
         <ItemCard />
         <ItemCard />
         <ItemCard />
